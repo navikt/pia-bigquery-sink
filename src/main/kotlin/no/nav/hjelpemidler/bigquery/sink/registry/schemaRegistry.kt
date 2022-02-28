41fc542ed00c0a2e24fa8e55a96c53ffab6ee38e
Erik Eiesland <erik.eiesland@nav.no>
2022-02-28 12:49:14 +0100

    legger tilbake hendelse_v1 i registry

--- src/main/kotlin/no/nav/hjelpemidler/bigquery/sink/registry/schemaRegistry.kt
+++ src/main/kotlin/no/nav/hjelpemidler/bigquery/sink/registry/schemaRegistry.kt
@@ -3,7 +3,7 @@ package no.nav.hjelpemidler.bigquery.sink.registry
 import no.nav.hjelpemidler.bigquery.sink.schema.SchemaDefinition
 
 val schemaRegistry: Map<SchemaDefinition.Id, SchemaDefinition> = mapOf(
-    //hendelse_v1.entry(),
+    hendelse_v1.entry(),
     hendelse_v2.entry(),
     //hjelpemiddelstatistikk_v1.entry(),
     tilbakeforing_gosys_tilbakemelding_v1.entry(),
