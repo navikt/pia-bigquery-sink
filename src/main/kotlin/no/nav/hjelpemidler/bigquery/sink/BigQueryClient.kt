b1a63910ad33d5533bcfafbf810a6af816048beb
Erik Eiesland <erik.eiesland@nav.no>
2022-03-09 16:33:26 +0100

    legger til støtte for nye kolonner i migrering

--- src/main/kotlin/no/nav/hjelpemidler/bigquery/sink/BigQueryClient.kt
+++ src/main/kotlin/no/nav/hjelpemidler/bigquery/sink/BigQueryClient.kt
@@ -2,7 +2,11 @@ package no.nav.hjelpemidler.bigquery.sink
 
 import com.google.cloud.bigquery.BigQueryOptions
 import com.google.cloud.bigquery.DatasetId
+import com.google.cloud.bigquery.FieldList
 import com.google.cloud.bigquery.InsertAllRequest.RowToInsert
+import com.google.cloud.bigquery.Schema
+import com.google.cloud.bigquery.Table
+import com.google.cloud.bigquery.TableDefinition
 import com.google.cloud.bigquery.TableId
 import com.google.cloud.bigquery.TableInfo
 import mu.KotlinLogging
@@ -13,6 +17,7 @@ interface BigQueryClient {
     fun datasetPresent(datasetId: DatasetId): Boolean
     fun tablePresent(tableId: TableId): Boolean
     fun create(tableInfo: TableInfo): TableInfo
+    fun update(tableId: TableId, updatedTableInfo: TableInfo): Boolean
     fun insert(tableId: TableId, row: RowToInsert)
 
     class BigQueryClientException(message: String) : RuntimeException(message)
@@ -28,6 +33,17 @@ class DefaultBigQueryClient(private val datasetId: DatasetId) : BigQueryClient {
         "projectId" to datasetId.project,
         "datasetId" to datasetId.dataset
     ) { block() }
+
+    private val TableInfo.schema: Schema
+        get() = requireNotNull(getDefinition<TableDefinition>().schema) {
+            "Tabell: '${tableId.table}' mangler skjema"
+        }
+
+    private val FieldList.names: Set<String> get() = map { it.name }.toSet()
+
+    private fun getTable(tableId: TableId): Table = requireNotNull(bigQuery.getTable(tableId)) {
+        "Mangler tabell: '${tableId.table}' i BigQuery"
+    }
 
     override fun datasetPresent(datasetId: DatasetId): Boolean = withLoggingContext {
         val present = bigQuery.getDataset(datasetId) != null
@@ -47,21 +63,43 @@ class DefaultBigQueryClient(private val datasetId: DatasetId) : BigQueryClient {
         createdTable
     }
 
-    override fun insert(tableId: TableId, row: RowToInsert) = withLoggingContext {
-        val tableName = tableId.table
-        val table = requireNotNull(bigQuery.getTable(tableId)) {
-            "Mangler tabell: '$tableName' i BigQuery"
+    override fun update(
+        tableId: TableId,
+        updatedTableInfo: TableInfo,
+    ): Boolean = withLoggingContext {
+        val table = getTable(tableId)
+        val existingFields = table.schema.fields.names
+        val updatedFields = updatedTableInfo.schema.fields.names
+        val newFields = updatedFields.minus(existingFields)
+        when {
+            newFields.isEmpty() -> {
+                log.info { "Ingen nye kolonner, ingen oppdatering av tabell: ${tableId.table} nødvendig" }
+                false
+            }
+            else -> {
+                log.info { "Legger til nye kolonner i tabell: ${tableId.table}, kolonner: $newFields" }
+                val updatedTable = table.toBuilder()
+                    .setDescription(updatedTableInfo.description)
+                    .setDefinition(updatedTableInfo.getDefinition())
+                    .build()
+                updatedTable.update()
+                true
+            }
         }
+    }
+
+    override fun insert(tableId: TableId, row: RowToInsert) = withLoggingContext {
+        val table = getTable(tableId)
         val rows = listOf(row)
         log.debug {
-            "Setter inn rader i tabell: '$tableName', rader: '$rows'"
+            "Setter inn rader i tabell: '${tableId.table}', rader: '$rows'"
         }
         val response = table.insert(rows)
         when {
             response.hasErrors() -> throw BigQueryClientException(
                 "Lagring i BigQuery feilet: '${response.insertErrors}'"
             )
-            else -> log.debug { "Rader ble lagret i tabell: '$tableName'" }
+            else -> log.debug { "Rader ble lagret i tabell: '${tableId.table}'" }
         }
     }
 
@@ -84,6 +122,11 @@ class LocalBigQueryClient : BigQueryClient {
     override fun create(tableInfo: TableInfo): TableInfo {
         log.info { "create(tableInfo) called with tableInfo: '$tableInfo'" }
         return tableInfo
+    }
+
+    override fun update(tableId: TableId, updatedTableInfo: TableInfo): Boolean {
+        log.info { "update(tableId, updatedTableInfo) called with tableId: '$tableId', updatedTableInfo: $updatedTableInfo" }
+        return true
     }
 
     override fun insert(tableId: TableId, row: RowToInsert) {
