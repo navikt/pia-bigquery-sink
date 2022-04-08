1f0b0aa3befaa310c0eb43f01cb1e6f410b883c9
Erik Eiesland <erik@eies.land>
2022-04-08 13:34:10 +0200

    oppgraderer kotlin og r-and-r deps til seneste
    pluss noen mindre deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val jacksonVersion = "2.13.1"
-
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.20"
     application
 }
 
@@ -12,28 +10,30 @@ repositories {
     maven("https://jitpack.io")
 }
 
-fun ktor(name: String) = "io.ktor:ktor-$name:1.6.7"
+fun ktor(name: String) = "io.ktor:ktor-$name:1.6.8"
 fun kotest(name: String) = "io.kotest:kotest-$name:5.1.0"
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
 
     // Ktor Server
     implementation(ktor("server-core"))
-    implementation(ktor("server-netty"))
+    implementation(ktor("server-cio"))
     implementation(ktor("metrics-micrometer"))
 
+    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-09.40.11a466d7ac70") {
+        exclude(group = "ch.qos.logback")
+    }
+
     implementation("com.natpryce:konfig:1.6.10.0")
-    implementation("com.github.navikt:rapids-and-rivers:2022.02.02-14.07.dc18de6a253c")
-    implementation("com.google.cloud:google-cloud-bigquery:2.9.0")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.8.2")
+    implementation("com.google.cloud:google-cloud-bigquery:2.10.5")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.8.4")
     implementation("org.reflections:reflections:0.10.2")
 
     // Logging
     implementation("io.github.microutils:kotlin-logging:2.1.21")
-    runtimeOnly("org.slf4j:slf4j-api:2.0.0-alpha6")
-    runtimeOnly("ch.qos.logback:logback-classic:1.3.0-alpha13")
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.11")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.0.1")
 
     // Testing
@@ -41,7 +41,7 @@ dependencies {
     testImplementation(ktor("server-test-host"))
     testImplementation(kotest("runner-junit5"))
     testImplementation(kotest("assertions-core"))
-    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("io.mockk:mockk:1.12.3")
 }
 
 application {
@@ -67,4 +67,3 @@ tasks.withType<Jar> {
         }
     )
 }
-
