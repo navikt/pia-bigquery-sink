d5d051322fb4a0d4921b01992d8465501ac3da13
Erik Eiesland <erik@eies.land>
2022-04-27 10:28:55 +0200

    oppgraderer r-and-r som gir ktor 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.6.21"
     application
 }
 
@@ -10,7 +10,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-fun ktor(name: String) = "io.ktor:ktor-$name:1.6.8"
+fun ktor(name: String) = "io.ktor:ktor-$name:2.0.0"
 fun kotest(name: String) = "io.kotest:kotest-$name:5.1.0"
 
 dependencies {
@@ -20,21 +20,21 @@ dependencies {
     // Ktor Server
     implementation(ktor("server-core"))
     implementation(ktor("server-cio"))
-    implementation(ktor("metrics-micrometer"))
+    implementation(ktor("server-metrics-micrometer"))
 
-    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-09.40.11a466d7ac70") {
+    implementation("com.github.navikt:rapids-and-rivers:2022.04.21-09.34.08966130226f") {
         exclude(group = "ch.qos.logback")
     }
 
     implementation("com.natpryce:konfig:1.6.10.0")
-    implementation("com.google.cloud:google-cloud-bigquery:2.10.5")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.8.4")
+    implementation("com.google.cloud:google-cloud-bigquery:2.10.10")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
     implementation("org.reflections:reflections:0.10.2")
 
     // Logging
     implementation("io.github.microutils:kotlin-logging:2.1.21")
     runtimeOnly("ch.qos.logback:logback-classic:1.2.11")
-    runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.0.1")
+    runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.1.1")
 
     // Testing
     testImplementation(kotlin("test"))
