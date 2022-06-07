4e07d5d279307bf6ac0f885f39530dbfe95a132f
Håkon <hakonhagen2@gmail.com>
2022-06-07 11:41:54 +0200

    La til inneholder_ikke_fritekst

--- src/main/kotlin/no/nav/hjelpemidler/bigquery/sink/registry/bestillingsordning_v3.kt
+++ src/main/kotlin/no/nav/hjelpemidler/bigquery/sink/registry/bestillingsordning_v3.kt
@@ -62,6 +62,9 @@ val bestillingsordning_v3 = object : SchemaDefinition {
             boolean("bruker_er_ikke_skjermet_person") {
                 description("Bruker er ikke skjermet person")
             }
+            boolean("inneholder_ikke_fritekst") {
+                description("Inneholder ikke fritekst")
+            }
             timestamp("tidsstempel") {
                 required()
                 description("Tidsstempel for lagring av hendelsen")
@@ -92,6 +95,7 @@ val bestillingsordning_v3 = object : SchemaDefinition {
         "bruker_bor_ikke_pa_institusjon" to (payload["bruker_bor_ikke_pa_institusjon"]?.asBoolean() ?: false),
         "bruker_bor_ikke_i_utlandet" to (payload["bruker_bor_ikke_i_utlandet"]?.asBoolean() ?: false),
         "bruker_er_ikke_skjermet_person" to (payload["bruker_er_ikke_skjermet_person"]?.asBoolean() ?: false),
+        "inneholder_ikke_fritekst" to (payload["inneholder_ikke_fritekst"]?.asBoolean() ?: false),
         "tidsstempel" to "AUTO",
     ).toRowToInsert()
 }
