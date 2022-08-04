bd869bca2e41aadaf97ecff299802baa17c1be62
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2022-08-04 09:13:28 +0200

    finpuss på avslag_v1. legg til description og opprettet+tidsstempel datetime

--- src/main/kotlin/no/nav/hjelpemidler/bigquery/sink/brille/avslag_v1.kt
+++ src/main/kotlin/no/nav/hjelpemidler/bigquery/sink/brille/avslag_v1.kt
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.module.kotlin.contains
 import com.google.cloud.bigquery.InsertAllRequest
 import com.google.cloud.bigquery.TableDefinition
 import com.google.cloud.bigquery.TimePartitioning
-import no.nav.hjelpemidler.bigquery.sink.asDate
 import no.nav.hjelpemidler.bigquery.sink.asDateTime
 import no.nav.hjelpemidler.bigquery.sink.registry.toRowToInsert
 import no.nav.hjelpemidler.bigquery.sink.schema.SchemaDefinition
@@ -22,24 +21,35 @@ val avslag_v1 = object : SchemaDefinition {
         schema {
             boolean("har_ikke_vedtak_i_kalenderaret_oppfylt") {
                 required()
+                description("Om barnet ikke allerede har et vedtak i samme kalenderår som bestillingsdato")
             }
             boolean("under_18_ar_pa_bestillingsdato_oppfylt") {
                 required()
+                description("Om barnet er under 18 år på bestillingsdato")
             }
             boolean("medlem_av_folketrygden_oppfylt") {
                 required()
+                description("Om barnet er medlem av folketrygden")
             }
             boolean("brillestyrke_oppfylt") {
                 required()
+                description("Om oppgitt brillestyrke er innenfor det ordningen dekker")
             }
             boolean("bestillingsdato_oppfylt") {
                 required()
+                description("Om bestillingsdato er etter ordningen startet, men ikke i fremtiden")
             }
             boolean("bestillingsdato_tilbake_i_tid_oppfylt") {
                 required()
+                description("Om bestillingsdato er max. 6 mnd tilbake i tid")
             }
-            date("opprettet") {
+            datetime("opprettet") {
                 required()
+                description("Tidsstempel for opprettelse av avtalen")
+            }
+            timestamp("tidsstempel") {
+                required()
+                description("Tidsstempel for lagring av avtalen")
             }
         }
         timePartitioning(TimePartitioning.Type.MONTH) {
@@ -51,13 +61,14 @@ val avslag_v1 = object : SchemaDefinition {
     }
 
     override fun transform(payload: JsonNode): InsertAllRequest.RowToInsert = mapOf(
-        payload.use("opprettet") { asDate() },
+        payload.use("opprettet") { asDateTime() },
         payload.use("har_ikke_vedtak_i_kalenderaret_oppfylt") { booleanValue() },
         payload.use("under_18_ar_pa_bestillingsdato_oppfylt") { booleanValue() },
         payload.use("medlem_av_folketrygden_oppfylt") { booleanValue() },
         payload.use("brillestyrke_oppfylt") { booleanValue() },
         payload.use("bestillingsdato_oppfylt") { booleanValue() },
-        payload.use("bestillingsdato_tilbake_i_tid_oppfylt") { booleanValue() }
+        payload.use("bestillingsdato_tilbake_i_tid_oppfylt") { booleanValue() },
+        "tidsstempel" to "AUTO"
     ).toRowToInsert()
 
     override fun skip(payload: JsonNode): Boolean = payload.contains("brilleseddel") // gammel event fra hm-brille-api
