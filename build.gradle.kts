8b3a75ed361801ef168f7aef5b4aeb1bcccca2b2
Christian Klem <christian.klem@nav.no>
2023-05-16 12:01:29 +0200

    Oppdater logback 1.4.6 -> 1.4.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 
     // Logging
     implementation("io.github.microutils:kotlin-logging:3.0.5")
-    runtimeOnly("ch.qos.logback:logback-classic:1.4.6")
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.7")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.3")
 
     // Testing
