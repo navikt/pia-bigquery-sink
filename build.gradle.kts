3ad88a9dfcf1acb7039ae1f8b9d6db64af1cb04d
Christian Klem <christian.klem@nav.no>
2026-08-03 14:55:22 +0200

    Fiks noen sårbarheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,20 +73,20 @@ dependencies {
 
     constraints {
         implementation("com.fasterxml.jackson.core:jackson-core") {
-            version { require("2.22.0") }
-            because("versjoner < 2.21.1 har sårbarhet. inkludert i ktor-server-auth:3.4.0")
+            version { require("2.22.1") }
+            because("versjoner < 2.22.1 har sårbarhet. inkludert i ktor-server-auth:3.4.0")
         }
         implementation("io.netty:netty-codec-http2") {
             version {
-                require("4.2.15.Final")
+                require("4.2.16.Final")
             }
             because(
-                "versjoner < 4.2.13.Final har sårbarhet. inkludert i ktor-server-netty-jvm:3.4.3",
+                "versjoner < 4.2.16.Final har sårbarhet. inkludert i ktor-server-netty-jvm:3.4.3",
             )
         }
         implementation("tools.jackson.core:jackson-core") {
-            version { require("3.2.0") }
-            because("versjoner < 3.1.0 har sårbarhet. inkludert i logstash-logback-encoder:9.0")
+            version { require("3.2.1") }
+            because("versjoner < 3.2.0 har sårbarhet. inkludert i logstash-logback-encoder:9.0")
         }
     }
 }
